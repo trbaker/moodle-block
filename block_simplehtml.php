841086597b6dcc5c9b7a8554bec911b3f8f94941
Tom Baker <trbaker@gmail.com>
2021-09-14 15:45:04 -0500

    up

--- block_simplehtml.php
+++ block_simplehtml.php
@@ -5,15 +5,35 @@ class block_simplehtml extends block_base {
     }
     // The PHP tag and the curly bracket for the class definition
     // will only be closed after there is another function added in the next section.
-  public function get_content() {
-    if ($this->content !== null) {
-      return $this->content;
-    }
+	public function get_content() {
+		if ($this->content !== null) {
+		  return $this->content;
+		}
 
-    $this->content         =  new stdClass;
-    $this->content->text   = 'The content of our SimpleHTML block!';
-    $this->content->footer = 'Footer here...';
 
-    return $this->content;
-}
+		$this->content         =  new stdClass;
+		if (! empty($this->config->text)) {
+				$this->content->text = $this->config->text;
+			} else {
+				$this->content->text   = 'The content of our SimpleHTML block!';
+			}
+
+		$this->content->footer = '';
+
+		return $this->content;
+	}
+	public function specialization() {
+		if (isset($this->config)) {
+			if (empty($this->config->title)) {
+				$this->title = get_string('defaulttitle', 'block_simplehtml');
+			} else {
+				$this->title = $this->config->title;
+			}
+
+			if (empty($this->config->text)) {
+				$this->config->text = get_string('defaulttext', 'block_simplehtml');
+			}
+		}
+	}
+
 }
