b2c1ad49f44872287a6c7e04a4e3972a7b6f74f5
Tom Baker <trbaker@gmail.com>
2021-09-15 16:19:48 -0500

    updated

--- README.md
+++
@@ -1,4 +0,0 @@
-# Moodle-block
-
- local path
- /Applications/MAMP/htdocs/moodle311/blocks
--- lang/en/block_simplehtml.php
+++
@@ -1,5 +0,0 @@
-<?php
-$string['pluginname'] = 'Simple HTML block';
-$string['simplehtml'] = 'Simple HTML';
-$string['simplehtml:addinstance'] = 'Add a new simple HTML block';
-$string['simplehtml:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
